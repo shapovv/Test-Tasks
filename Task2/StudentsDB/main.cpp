47faddd54abb3b5ea4ab36fa7c3336b5620291c7
shapovv <asshapovalov@yandex.ru>
2023-12-21 10:02:55 +0300

    Work is debugged and bugs are fixed

--- Task2/StudentsDB/main.cpp
+++ Task2/StudentsDB/main.cpp
@@ -5,13 +5,15 @@
 #include <algorithm>
 #include <vector>
 
+// Использование пространств имен для упрощения кода
 using namespace web;
 using namespace web::http;
 using namespace web::http::experimental::listener;
 
-sqlite3 *db;
-char *errMessage = 0;
+sqlite3 *db;  // Глобальный указатель на объект базы данных SQLite
+char *errMessage = 0;  // Глобальная переменная для сообщений об ошибках SQLite
 
+// Функция для инициализации и открытия базы данных
 bool initDB(const std::string &dbPath) {
     if (sqlite3_open(dbPath.c_str(), &db) != SQLITE_OK) {
         std::cerr << "Error opening SQLite DB: " << sqlite3_errmsg(db) << std::endl;
@@ -20,6 +22,7 @@ bool initDB(const std::string &dbPath) {
     return true;
 }
 
+// Функция для выполнения SQL-запросов с параметрами
 bool executeSQL(const std::string &sql, const std::vector<std::string> &params = {}) {
     sqlite3_stmt *stmt;
     if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL) != SQLITE_OK) {
@@ -41,14 +44,17 @@ bool executeSQL(const std::string &sql, const std::vector<std::string> &params =
     return true;
 }
 
+// Валидация входных данных для предотвращения SQL инъекций
 bool isValidInput(const std::string &input) {
     return input.find("'") == std::string::npos;
 }
 
+// Обработчик GET-запросов для получения списка студентов
 void handle_get(http_request request) {
     std::cout << "GET request received" << std::endl;
 
-    std::string sql = "SELECT id, first_name, last_name, middle_name, birth_date, group_name FROM students";
+    std::string sql =
+        "SELECT id, first_name, last_name, middle_name, birth_date, group_name FROM students";
     sqlite3_stmt *stmt;
     if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL) != SQLITE_OK) {
         std::cerr << "Failed to prepare statement" << std::endl;
@@ -65,12 +71,18 @@ void handle_get(http_request request) {
     int index = 0;
     while (sqlite3_step(stmt) == SQLITE_ROW) {
         json::value student = json::value::object();
+        // Поочередное извлечение данных о студенте и добавление их в JSON-объект
         student[U("id")] = json::value::number(sqlite3_column_int(stmt, 0));
-        student[U("first_name")] = json::value::string(U(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1))));
-        student[U("last_name")] = json::value::string(U(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 2))));
-        student[U("middle_name")] = json::value::string(U(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 3))));
-        student[U("birth_date")] = json::value::string(U(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 4))));
-        student[U("group_name")] = json::value::string(U(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 5))));
+        student[U("first_name")] =
+            json::value::string(U(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 1))));
+        student[U("last_name")] =
+            json::value::string(U(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 2))));
+        student[U("middle_name")] =
+            json::value::string(U(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 3))));
+        student[U("birth_date")] =
+            json::value::string(U(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 4))));
+        student[U("group_name")] =
+            json::value::string(U(reinterpret_cast<const char *>(sqlite3_column_text(stmt, 5))));
         students[index++] = student;
     }
 
@@ -78,53 +90,62 @@ void handle_get(http_request request) {
 
     http_response response(status_codes::OK);
     response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
-    response.set_body(students);
+    response.set_body(students);  // Отправка списка студентов клиенту
     request.reply(response);
 }
 
-
+// Обработчик POST-запросов для добавления нового студента
 void handle_post(http_request request) {
-    request.extract_json().then([=](json::value requestJson) {
-      std::string firstName = requestJson[U("first_name")].as_string();
-      std::string lastName = requestJson[U("last_name")].as_string();
-      std::string middleName = requestJson[U("middle_name")].as_string();
-      std::string birthDate = requestJson[U("birth_date")].as_string();
-      std::string groupName = requestJson[U("group_name")].as_string();
+    request.extract_json()
+        .then([=](json::value requestJson) {
+            // Извлечение данных о студенте из JSON-запроса
+            std::string firstName = requestJson[U("first_name")].as_string();
+            std::string lastName = requestJson[U("last_name")].as_string();
+            std::string middleName = requestJson[U("middle_name")].as_string();
+            std::string birthDate = requestJson[U("birth_date")].as_string();
+            std::string groupName = requestJson[U("group_name")].as_string();
 
-      if (!isValidInput(firstName) || !isValidInput(lastName) || !isValidInput(middleName) || !isValidInput(birthDate)
-          || !isValidInput(groupName)) {
-          http_response response(status_codes::BadRequest);
-          response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
-          response.set_body(U("Invalid input data"));
-          request.reply(response);
-          return;
-      }
+            // Проверка валидности входных данных
+            if (!isValidInput(firstName) || !isValidInput(lastName) || !isValidInput(middleName) ||
+                !isValidInput(birthDate) || !isValidInput(groupName)) {
+                http_response response(status_codes::BadRequest);
+                response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
+                response.set_body(U("Invalid input data"));
+                request.reply(response);
+                return;
+            }
 
-      std::string sql =
-          "INSERT INTO students (first_name, last_name, middle_name, birth_date, group_name) VALUES (?, ?, ?, ?, ?)";
-      std::vector<std::string> params = {firstName, lastName, middleName, birthDate, groupName};
+            std::string sql =
+                "INSERT INTO students (first_name, last_name, middle_name, birth_date, group_name) "
+                "VALUES (?, ?, ?, ?, ?)";
+            std::vector<std::string> params = {firstName, lastName, middleName, birthDate,
+                                               groupName};
 
-      if (!executeSQL(sql, params)) {
-          http_response response(status_codes::InternalError);
-          response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
-          response.set_body(U("Failed to insert student"));
-          request.reply(response);
-          return;
-      }
+            // Выполнение SQL-запроса на добавление студента
+            if (!executeSQL(sql, params)) {
+                http_response response(status_codes::InternalError);
+                response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
+                response.set_body(U("Failed to insert student"));
+                request.reply(response);
+                return;
+            }
 
-      std::cout << "POST request received: " << requestJson.serialize() << std::endl;
-      http_response response(status_codes::OK);
-      response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
-      response.set_body(U("Student added successfully"));
-      request.reply(response);
-    }).wait();
+            std::cout << "POST request received: " << requestJson.serialize() << std::endl;
+            http_response response(status_codes::OK);
+            response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
+            response.set_body(U("Student added successfully"));
+            request.reply(response);
+        })
+        .wait();
 }
 
+// Обработчик DELETE-запросов для удаления студента
 void handle_delete(http_request request) {
     auto path = uri::split_path(uri::decode(request.relative_uri().path()));
     if (path.size() == 2 && path[0] == "students") {
         std::string studentId = path[1];
 
+        // Проверка, является ли ID студента числом
         if (!std::all_of(studentId.begin(), studentId.end(), ::isdigit)) {
             http_response response(status_codes::BadRequest);
             response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
@@ -133,7 +154,7 @@ void handle_delete(http_request request) {
             return;
         }
 
-        // Проверка, существует ли студент с таким ID
+        // Проверка существования студента в базе данных
         std::string checkSql = "SELECT COUNT(*) FROM students WHERE id = ?";
         sqlite3_stmt *stmt;
         if (sqlite3_prepare_v2(db, checkSql.c_str(), -1, &stmt, NULL) != SQLITE_OK) {
@@ -146,7 +167,7 @@ void handle_delete(http_request request) {
         sqlite3_bind_text(stmt, 1, studentId.c_str(), -1, SQLITE_TRANSIENT);
 
         if (sqlite3_step(stmt) != SQLITE_ROW || sqlite3_column_int(stmt, 0) == 0) {
-            // Студент с таким ID не найден
+            // Если студент не найден, отправляем соответствующий ответ
             http_response response(status_codes::NotFound);
             response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
             response.set_body(U("Student not found"));
@@ -156,7 +177,7 @@ void handle_delete(http_request request) {
         }
         sqlite3_finalize(stmt);
 
-        // Продолжение удаления студента, если он существует
+        // Выполнение SQL-запроса на удаление студента
         std::string sql = "DELETE FROM students WHERE id = ?";
         if (!executeSQL(sql, {studentId})) {
             http_response response(status_codes::InternalError);
@@ -178,14 +199,13 @@ void handle_delete(http_request request) {
     }
 }
 
-
-
+// Функция закрытия базы данных
 void closeDB() {
     sqlite3_close(db);
 }
 
-void handle_options(http_request request)
-{
+// Обработчик OPTIONS запросов, необходимый для работы CORS
+void handle_options(http_request request) {
     http_response response(status_codes::OK);
     response.headers().add(U("Allow"), U("GET, POST, OPTIONS, DELETE"));
     response.headers().add(U("Access-Control-Allow-Origin"), U("*"));
@@ -194,36 +214,39 @@ void handle_options(http_request request)
     request.reply(response);
 }
 
-
-
-
+// Основная функция для инициализации и запуска HTTP-сервера
 int main() {
     std::string dbPath = "StudentsDB.sqlite";
 
-
+    // Инициализация и открытие базы данных
     if (!initDB(dbPath)) {
         std::cerr << "Failed to open database." << std::endl;
         return 1;
     }
 
+    // Настройка HTTP-слушателя (listener) для обработки входящих запросов
     http_listener listener(U("http://localhost:6502"));
 
+    // Регистрация обработчиков для различных типов HTTP-запросов
     listener.support(methods::GET, handle_get);
     listener.support(methods::POST, handle_post);
     listener.support(methods::DEL, handle_delete);
     listener.support(methods::OPTIONS, handle_options);
 
     try {
-        listener.open().then([&listener]() { std::cout << "Server is listening on port 6502...\n"; }).wait();
+        // Запуск слушателя и ожидание входящих запросов
+        listener.open()
+            .then([&listener]() { std::cout << "Server is listening on port 6502...\n"; })
+            .wait();
         std::cout << "Press Enter to exit." << std::endl;
         std::string dummy;
         std::getline(std::cin, dummy);
         listener.close().wait();
-    }
-    catch (std::exception const &e) {
+    } catch (std::exception const &e) {
         std::cout << e.what() << std::endl;
     }
 
+    // Закрытие базы данных при завершении работы сервера
     closeDB();
 
     return 0;
